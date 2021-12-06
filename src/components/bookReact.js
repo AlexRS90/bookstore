74fb0e1452c69a4d28f367cc072bf265004d8f0a
AlexRS90 <aramosbengy@gmail.com>
2021-12-06 16:16:24 -0600

    Inicialize project

---
+++ src/components/bookReact.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const dummyBooks = [
+  {
+    id: 123,
+    title: 'This is a book title',
+    auhor: 'Alex',
+  },
+];
+
+const DisplayBook = () => {
+  const [book, setBook] = useState(dummyBooks);
+  return (
+    <div>
+      <section>
+          <p>{book[0].title}</p>
+          <ul>
+            <li>Comments</li>
+            <li>Remove</li>
+            <li>Edit</li>
+          </ul>
+      </section>
+      <section>
+        <form>
+          <h2>Add a new book</h2>
+          <input type="text" placeholder="Book Title" />
+          <select name="Categories">
+            <option>Aciton</option>
+            <option>Races</option>
+          </select>
+          <input type="submit" value="Add Book" />
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default DisplayBook;
