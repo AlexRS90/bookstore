ac0b4be73657971112b15745e2dc88cef5a62702
AlexRS90 <aramosbengy@gmail.com>
2021-12-08 13:07:36 -0600

    Add preventDefault()

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -7,30 +7,31 @@ const AddNewBook = () => {
   const dispatch = useDispatch();
   return (
     <section>
-      <form>
+      <form onSubmit={(e) => submitBookToStore(e, dispatch)}>
         <h2>Add a new book</h2>
-        <input type="text" id="book-title" placeholder="Book Title" />
-        <input type="text" id="book-author" placeholder="Book Author" />
+        <input type="text" id="book-title" required placeholder="Book Title" />
+        <input type="text" id="book-author" required placeholder="Book Author" />
         <select name="Categories">
           <option>Action</option>
           <option>Drama</option>
           <option>Scy-Fi</option>
         </select>
-        <button type="submit" onClick={() => submitBookToStore(dispatch)}>Add Book</button>
+        <button type="submit">Add Book</button>
       </form>
     </section>
   );
 };
 
-const submitBookToStore = (dispatch) => {
+const submitBookToStore = (e, dispatch) => {
+  e.preventDefault();
   const newBook = {
     id: uuidv4(),
     title: document.getElementById('book-title').value,
     author: document.getElementById('book-author').value,
   }
-  dispatch(addBook(newBook));
   document.getElementById('book-title').value = '';
   document.getElementById('book-author').value = '';
+  dispatch(addBook(newBook));
 };
 
 export default AddNewBook;
