d5765c5c39f23cd0c1002dc9923a9e46441b9dda
AlexRS90 <aramosbengy@gmail.com>
2021-12-08 12:17:22 -0600

    Add provider from Redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './App.css';
 import Navbar from './components/navbar';
 import Books from './components/bookReact';
@@ -7,11 +9,13 @@ import Categories from './components/categories';
 
 const App = () => (
   <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/Categories" element={<Categories />} />
-    </Routes>
+    <Provider store={store}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Provider>
   </>
 
 );
