652c8384dff30a7ce1544a8017422b8b6f1bda05
AlexRS90 <aramosbengy@gmail.com>
2021-12-08 13:34:50 -0600

    Add Proptype definition

--- src/components/deleteBook.js
+++ src/components/deleteBook.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
 const DeleteButton = (props) => {
   const dispatch = useDispatch();
-  const {id} = props;
+  const { id } = props;
   return (
     <div>
       <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
     </div>
   );
 };
+
+DeleteButton.prototype = {
+  props: PropTypes.string.isRequired,
+}
 
 export default DeleteButton;
