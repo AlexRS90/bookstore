e7b0ab1b9b0ed9e1205899d1d96336429c0f3f3b
AlexRS90 <aramosbengy@gmail.com>
2021-12-08 12:19:13 -0600

    Add book to Redux state

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
+import { v4 as uuidv4 } from "uuid";
 
-const AddNewBook = () => (
-  <section>
-    <form>
-      <h2>Add a new book</h2>
-      <input type="text" placeholder="Book Title" />
-      <select name="Categories">
-        <option>Action</option>
-        <option>Drama</option>
-        <option>Scy-Fi</option>
-      </select>
-      <button type="submit">Add Book</button>
-    </form>
-  </section>
-);
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+  return (
+    <section>
+      <form>
+        <h2>Add a new book</h2>
+        <input type="text" id="book-title" placeholder="Book Title" />
+        <input type="text" id="book-author" placeholder="Book Author" />
+        <select name="Categories">
+          <option>Action</option>
+          <option>Drama</option>
+          <option>Scy-Fi</option>
+        </select>
+        <button type="submit" onClick={() => submitBookToStore(dispatch)}>Add Book</button>
+      </form>
+    </section>
+  );
+};
+
+const submitBookToStore = (dispatch) => {
+  const newBook = {
+    id: uuidv4(),
+    title: document.getElementById('book-title').value,
+    author: document.getElementById('book-author').value,
+  }
+  dispatch(addBook(newBook));
+  document.getElementById('book-title').value = '';
+  document.getElementById('book-author').value = '';
+};
 
 export default AddNewBook;
