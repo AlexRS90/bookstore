04257b9c5cb261292b986c71a021cb39af4d09f0
AlexRS90 <aramosbengy@gmail.com>
2021-12-10 14:14:14 -0600

    Add HTML classes

--- src/components/bookReact.js
+++ src/components/bookReact.js
@@ -3,46 +3,47 @@ import { useSelector, useDispatch } from 'react-redux';
 import AddNewBook from './addBook';
 import DeleteButton from './deleteBook';
 import { fetchBooks } from '../redux/books/books';
+import './bookReact.css';
 
 const DisplayBook = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchBooks());
   }, []);
-  const ulMenu = {
-    display: 'flex',
-    listStyle: 'none',
-    gap: '20px',
-    alignItems: 'center',
-  };
-  const deleteButton = {
-    border: '2px solid teal',
-    padding: '5px',
-    cursor: 'pointer',
-  };
-  const wrapper = {
-    border: '2px solid grey',
-    marginBottom: '10px',
-  };
   const bookStore = useSelector((state) => state.book.books);
   return (
     <div>
       <section>
         {bookStore.map((book) => (
-          <div key={book.id} style={wrapper}>
-            <p>
-              Title:
-              {book.title}
-            </p>
-            <p>
-              Category:
-              {book.category}
-            </p>
-            <ul style={ulMenu}>
-              <li>Comments</li>
-              <li style={deleteButton}><DeleteButton id={book.id} /></li>
-              <li>Edit</li>
-            </ul>
+          <div key={book.id} className="wrapper">
+            <div className="book-info">
+              <p className="category">
+                {book.category}
+              </p>
+              <p className="title">
+                {book.title}
+              </p>
+              <p className="author">
+                Anonymous
+              </p>
+              <ul className="ulMenu">
+                <li>Comments</li>
+                <li><DeleteButton id={book.id} /></li>
+                <li>Edit</li>
+              </ul>
+            </div>
+            <div className="progress">
+              <div className="circle"></div>
+              <div>
+                <p className="percentage">75%</p>
+                <p className="progress-text">Completed</p>
+              </div>
+            </div>
+            <div className="update-progress">
+              <p className="current-chap">CURRENT CHAPTER</p>
+              <p className="chapter">Chapter 8</p>
+              <button className="update-button">UPDATE PROGRESS</button>
+            </div>
           </div>
         ))}
       </section>
